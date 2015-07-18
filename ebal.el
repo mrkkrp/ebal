b5987b7f278f715faff5be3b17e91b6bb6e37356
mrkkrp <markkarpov@opmbx.org>
2015-07-18 19:55:06 +0600

    settings & variables written

--- ebal.el
+++ ebal.el
@@ -33,6 +33,141 @@
 (require 'f)
 (require 'mmt)
 
+;; Settings & Variables
+
+(defgroup ebal nil
+  "Emacs interface to Cabal."
+  :group 'programming
+  :tag "Ebal"
+  :prefix "ebal-")
+
+(defvar ebal--command-alist nil
+  "Alist that maps names of commands to functions to call.
+
+This variable is modified by `ebal-define-command' when some
+command is defined.  Do not modify it manually unless you know
+what you're doing.")
+
+(defvar ebal--active-command nil
+  "Name of currently performed command (symbol) or NIL.
+
+NIL value means that no command is performed right now.  This is
+set by Ebal before running of “before” command hooks and reset
+after running “after” command hook.
+
+This variable is mainly useful when you want to add
+‘ebal-before-command-hook’ or ‘ebal-after-command-hook’ and you
+need to test which command is currently performed.")
+
+(defvar ebal--last-directory nil
+  "Path to project's directory last time `ebal-execute' was called.
+
+This is mainly used to check when we need to reload/re-parse
+project-local settings that user might have.")
+
+(defcustom ebal-cabal-executable nil
+  "Path to cabal executable.
+
+If it's not NIL, this value is used in invocation of Cabal
+commands instead of standard \"cabal\" string.  Set this variable
+if your Cabal is in a strange place where OS cannot find it.
+
+Note that the path is quoted with `shell-quote-argument' before
+being used to compose command line."
+  :tag "Path to Cabal Executable"
+  :type '(choice (file :must-match t)
+                 (const :tag "Use Default" nil)))
+
+(defcustom ebal-global-options nil
+  "Alist that maps names of commands to their default options.
+
+Names of commands are symbols and options are strings.  If option
+string is missing for some command or it's NIL, this results in
+empty string.
+
+Note that this is global collection of options.  If you want to
+specify option to be used only with a specific command and in a
+specific project, see `ebal-project-options' and corresponding
+setup instructions."
+  :tag "Global Options for Ebal Commands"
+  :type '(alist :key-type symbol
+                :value-type (string :tag "Command Line Options")))
+
+(defcustom ebal-project-options nil
+  "Alist that maps names of commands to their default options.
+
+Names of commands are symbols and options are strings.  If option
+string is missing for some command or it's NIL, this results in
+empty string.
+
+This variable represents user's preferences for current project.
+Value of the variable is read from \"*.ebal\" file that may be
+present in project's root directory (the same directory that
+contains \"*.cabal\" file)."
+  :tag "Project Specific Options"
+  :type '(alist :key-type symbol
+                :value-type (string :tag "Command Line Options")))
+
+(defcustom ebal-command-dependency t
+  "Whether or not perform dependencies of commands.
+
+Since typical usage of Cabal involves a lot of boilerplate, Ebal
+allows to perform some ritual actions for user, so he/she has no
+chances to forget something.
+
+NIL value of this variable tells Ebal be not so smart and do only
+what's explicitly asked."
+  :tag "Allow Command Dependencies"
+  :type 'boolean)
+
+(defcustom ebal-sandboxing 'ask
+  "This determines Ebal's policy towards sandboxing.
+
+The following values are recognized:
+
+NIL — don't create sandboxes unless user explicitly runs command
+to create one.
+
+ask — ask if user wants to create a sandbox (so it's harder to
+forget to create it), this is often preferable because most
+Haskell developers want sandboxes everywhere nowadays (default).
+
+always — create sandboxes silently when they are missing and they
+should be created.  With this option every your project is
+sandboxed without any effort on your side.
+
+All other values of this variabe produce the same effect as
+`always'."
+  :tag "Sandboxing Policy"
+  :type '(choice (const :tag "User creates sandboxes manually" nil)
+                 (const :tag "Ask whether to create one" ask)
+                 (const :tag "Silently create sandboxes" always)))
+
+(defcustom ebal-bury-on-success nil
+  "Wheter to bury compilation bury on success.
+
+If this variable is bound to non-NIL value, restore window
+configuration after successful execution of Ebal command."
+  :tag "Bury *Compilation* buffer on success"
+  :type 'boolean)
+
+(defcustom ebal-completing-read-function #'ebal-built-in-completing-read
+  "Function to be called when requesting input from the user."
+  :tag "Completing Function"
+  :type '(radio (function-item ebal-built-in-completing-read)
+                (function-item ebal-ido-completing-read)))
+
+(defcustom ebal-select-command-function #'ebal-command-popup
+  "Function to call to select Ebal command.
+
+This is what `ebal-execute' uses.  Default is Ebal custom popup
+buffer, but you can use IDO-powered variant if you like or plain
+`ebal-command-completing-read'."
+  :tag "How to Select Command"
+  :type '(radio (function-item ebal-command-popup)
+                (function-item ebal-command-ido)
+                (function-item ebal-command-completing-read)))
+
 ;; TODO: this is just a batch of hacks, write great package now
 
 (defvar ebal-cabal-operations
