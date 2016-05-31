1155c70b8dbb2f2caed2958782afe3d8018ab0b2
mrkkrp <markkarpov@opmbx.org>
2016-05-31 23:06:21 +0700

    Emacs loves two whitespaces after a period

--- ebal.el
+++ ebal.el
@@ -25,7 +25,7 @@
 
 ;;; Commentary:
 
-;; This is Emacs interface to Cabal and Stack. Currently, it provides fast
+;; This is Emacs interface to Cabal and Stack.  Currently, it provides fast
 ;; and easy access to most Cabal commands († — commands available in Stack
 ;; mode):
 ;;
