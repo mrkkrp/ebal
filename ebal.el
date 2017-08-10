4d19565516785348894c4911e757e33a270b3efd
mrkkrp <markkarpov@opmbx.org>
2017-08-10 20:31:27 +0700

    Version bump (0.3.1)

--- ebal.el
+++ ebal.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Mark Karpov <markkarpov92@gmail.com>
 ;; URL: https://github.com/mrkkrp/ebal
-;; Version: 0.3.0
+;; Version: 0.3.1
 ;; Package-Requires: ((emacs "24.4") (f "0.18.0"))
 ;; Keywords: convenience, cabal, haskell
 ;;
